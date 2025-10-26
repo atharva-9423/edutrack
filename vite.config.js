ec4c09af9c98cc19166cee19792014710b600257
Atharva <phatangareatharva@gmail.com>
2025-10-26 18:36:18 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +8,13 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 5000,
     hmr: true,
-  }
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        admin: resolve(__dirname, 'admin.html'),
+      },
+    },
+  },
 })
